dba58a4d2f71d2f340fa1160b1ade77ac2b31563
David Gaden <dlfgaden@gmail.com>
2021-05-26 14:14:52 -0500

    Upgraded to Unity 2020.3.9f1, added SpriteMerge from unity forums, need to modify

--- Assets/Scripts/LadderHandler.cs
+++ Assets/Scripts/LadderHandler.cs
@@ -22,6 +22,7 @@ public class LadderHandler : MonoBehaviour
     [Tooltip("A mask determining what is a ladder")]
     [SerializeField] private LayerMask m_ladderLayers;
 
+
     [Header("What Is Ground?")]
     [SerializeField] private bool m_specifyLayerMask = false;
     [SerializeField] private LayerMask m_groundLayers;
@@ -50,48 +51,49 @@ public class LadderHandler : MonoBehaviour
         m_ladderGrid = m_ladderMap.layoutGrid;
 
         ObjectFinder groundFinder = new ObjectFinder();
-
-        if (m_specifyLayerMask)
-        {
-            groundFinder.SetLayerMask(m_groundLayers);
-        }
-        if (m_specifyMinDepth)
-        {
-            groundFinder.SetMinDepth(m_groundMinDepth);
-        }
-        if (m_specifyMaxDepth)
-        {
-            groundFinder.SetMaxDepth(m_groundMaxDepth);
-        }
-        if (m_specifyNameContains)
-        {
-            groundFinder.SetNameContains(m_groundNameContains);
-        }
-        if (m_specifyNameExact)
-        {
-            groundFinder.SetNameExact(m_groundNameExact);
-        }
-        if (m_specifyTagContains)
-        {
-            groundFinder.SetTagContains(m_groundTagContains);
-        }
-        if (m_specifyTagExact)
-        {
-            groundFinder.SetTagExact(m_groundTagExact);
-        }
-
-        m_groundObject = groundFinder.FindObject();
-        if (m_groundObject)
-        {
-            m_groundMap = m_groundObject.GetComponent<Tilemap>();
-            if (m_groundMap)
+        { // ObjectFinder scope for code-folding
+            if (m_specifyLayerMask)
             {
-                m_groundGrid = m_groundMap.layoutGrid;
-                m_groundCollider = m_groundObject.GetComponent<TilemapCollider2D>();
-                if (m_groundCollider)
+                groundFinder.SetLayerMask(m_groundLayers);
+            }
+            if (m_specifyMinDepth)
+            {
+                groundFinder.SetMinDepth(m_groundMinDepth);
+            }
+            if (m_specifyMaxDepth)
+            {
+                groundFinder.SetMaxDepth(m_groundMaxDepth);
+            }
+            if (m_specifyNameContains)
+            {
+                groundFinder.SetNameContains(m_groundNameContains);
+            }
+            if (m_specifyNameExact)
+            {
+                groundFinder.SetNameExact(m_groundNameExact);
+            }
+            if (m_specifyTagContains)
+            {
+                groundFinder.SetTagContains(m_groundTagContains);
+            }
+            if (m_specifyTagExact)
+            {
+                groundFinder.SetTagExact(m_groundTagExact);
+            }
+
+            m_groundObject = groundFinder.FindObject();
+            if (m_groundObject)
+            {
+                m_groundMap = m_groundObject.GetComponent<Tilemap>();
+                if (m_groundMap)
                 {
-                    m_groundHandlingEnabled = true;
-                    Debug.Log("My ground GameObject is named " + m_groundObject.name);
+                    m_groundGrid = m_groundMap.layoutGrid;
+                    m_groundCollider = m_groundObject.GetComponent<TilemapCollider2D>();
+                    if (m_groundCollider)
+                    {
+                        m_groundHandlingEnabled = true;
+                        Debug.Log("My ground GameObject is named " + m_groundObject.name);
+                    }
                 }
             }
         }
@@ -101,28 +103,31 @@ public class LadderHandler : MonoBehaviour
 
     // Access
 
-    public Grid LadderGrid()
-    {
-        return m_ladderGrid;
-    }
-    public Tilemap LadderMap()
-    {
-        return m_ladderMap;
-    }
-    public TilemapCollider2D LadderMapCollider()
-    {
-        return m_ladderCollider;
-    }
+    public Grid LadderGrid() { return m_ladderGrid; }
+    public Tilemap LadderMap() { return m_ladderMap; }
+    public TilemapCollider2D LadderMapCollider() { return m_ladderCollider; }
 
 
     // Query
 
+    // Passive ladder operations - does not need ground
+
+    /// <summary>
+    /// Tests if the given collider overlaps a ladder
+    /// </summary>
+    /// <param name="testCollider">Collider to check for ladder overlaps</param>
+    /// <returns>true if collider overlaps a ladder</returns>
     public bool OnLadder(Collider2D testCollider)
     {
         return testCollider.IsTouching(m_ladderCollider);
     }
 
-
+    /// <summary>
+    /// Finds all ladders overlapping testCollider, and returns the one that is closest to the testCollider's centre bounds
+    /// </summary>
+    /// <param name="testCollider"></param>
+    /// <param name="closestPosition">gets set to the 2D mid-point of the closest ladder tile</param>
+    /// <returns>true if a ladder was found</returns>
     public bool ClosestLadder(Collider2D testCollider, ref Vector2 closestPosition)
     {
         if (!OnLadder(testCollider))
@@ -159,13 +164,13 @@ public class LadderHandler : MonoBehaviour
         float minDist = float.MaxValue;
         Vector2 bestCoords = Vector2.zero;
         bool foundLadder = false;
-        Vector3 halfWay = m_ladderGrid.cellSize * 0.5f;
+        //Vector3 halfWay = m_ladderGrid.cellSize * 0.5f;
         Vector2 testCtr = testCollider.bounds.center;
         foreach (Vector3Int cellI in neighbours)
         {
             if (m_ladderMap.HasTile(cellI))
             {
-                Vector2 ladderCoords = m_ladderGrid.CellToWorld(cellI) + halfWay;
+                Vector2 ladderCoords = m_ladderMap.GetCellCenterWorld(cellI);// m_ladderGrid.CellToWorld(cellI) + halfWay;
                 float curDist = Vector2.Distance(testCtr, ladderCoords);
                 if (curDist < minDist)
                 {
@@ -184,15 +189,64 @@ public class LadderHandler : MonoBehaviour
         return foundLadder;
     }
 
+
     private void ResetTrapdoors()
     {
+        if (!m_groundHandlingEnabled)
+        {
+            return;
+        }
         BoundsInt cellBounds = m_ladderMap.cellBounds;
         for (int x = cellBounds.min.x; x <= cellBounds.max.x; ++x)
         {
+            // At top, all ladder tiles need trapdoors
+            Vector3Int curLadderCell = new Vector3Int(x, cellBounds.max.y, 0);
+            if (m_ladderMap.HasTile(curLadderCell))
+            {
+                AddLadderTop(curLadderCell);
+            }
             for (int y = cellBounds.max.y-1; y >= cellBounds.min.y; --y)
             {
                 
             }
         }
+    }
+
+
+    private void AddLadderTop(Vector3Int ladderCell)
+    {
+        Vector3Int groundCoords = LadderCellToGroundCell(ladderCell);
+        if (m_groundMap.HasTile(groundCoords))
+        {
+            // We need to combine ground & ladder tiles together
+        }
+    }
+
+    struct TrapDoor
+    {
+        /// <summary>
+        /// Coordinates in the ladder map of the TrapDoor
+        /// </summary>
+        public Vector3Int ladderCell { get; set; }
+        /// <summary>
+        /// Tile to put on ladderMap when opening for MegaMan to pass
+        /// If a tile exists on both ground and ladder maps, this should be a pixel-by-pixel combination, otherwise it should just be a copy of the
+        /// tile from the ladder map
+        /// </summary>
+        /// <value></value>
+        public Tile openTile { get; set; }
+    }
+
+
+
+    private Vector3Int LadderCellToGroundCell(Vector3Int ladderCell)
+    {
+        return m_groundGrid.WorldToCell(m_ladderGrid.CellToWorld(ladderCell));
+    }
+
+
+    private Vector3Int GroundCellToLadderCell(Vector3Int groundCell)
+    {
+        return m_ladderGrid.WorldToCell(m_groundGrid.CellToWorld(groundCell));
     }
 }
