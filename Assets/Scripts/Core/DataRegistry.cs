041bbf0cef8e8156be1b1a84a496a5f90957806b
David Gaden <dlfgaden@gmail.com>
2021-06-30 22:30:09 -0500

    Continuing work on DataRegistry

--- Assets/Scripts/Core/DataRegistry.cs
+++ Assets/Scripts/Core/DataRegistry.cs
@@ -26,6 +26,7 @@ public static class DataRegistryRoot {
 
 /// <summary>
 /// A basic header for objects that can be stored in the DataRegistry.  Other objects may depend on data in this object.
+/// This can be attached to the actual object, or a seperate header used to refer to that object
 /// </summary>
 public interface IDataObject {
     /// <summary>
@@ -41,26 +42,55 @@ public interface IDataObject {
     /// </summary>
     /// <value></value>
     public ModTag MTag { get; }
+    /// <summary>
+    /// The DataRegistry that I'm registered in, or null if orphan
+    /// </summary>
+    public IDataRegistry Parent { get; }
+    /// Convert this to an IDerivedDataObject, or null if it is not one.
+    /// </summary>
+    /// <returns></returns>
+    public IDerivedDataObject DerivedDataObject();
+    /// <summary>
+    /// Return a header-only IDataObject copy of this object
+    /// </summary>
+    public IDataObject CloneHeader();
 }
 
 /// <summary>
 /// Additional interface components for DataObjects that depend on other data objects.
 /// I.e. these objects have 'derived' data.
 /// </summary>
-public interface IDerivedDataObject {
+public interface IDerivedDataObject : IDataObject {
     /// <summary>
     /// Other data objects on which this one depends
     /// </summary>
     public List<IDataObject> DependsOn { get; }
-    // TODO - What else?
+    /// <summary>
+    /// Bring all my derived data up to date
+    /// </summary>
+    public void Update();
+    /// <summary>
+    /// True if I am up to date
+    /// </summary>
+    public bool UpToDate();
 }
 
-public interface IDataRegistry {
-
+public interface IDataRegistry : IDataObject {
+    public IDataObject FindDataObject(long id, bool recursive=true);
+    public List<IDataObject> FindDataObjects(string name, bool recursive=true);
+    public List<string> TableOfContents(bool recursive=true);
+    /// <summary>
+    /// List of all child objects' ids, optionally recursive.
+    /// </summary>
+    public List<long> Index(bool recursive=true);
+    public void Register(IDataObject obj);
+    public bool Unregister(IDataObject obj);
+    public bool Unregister(long id);
 }
 
 /// <summary>
-/// Modification tag, assigned incrementally.  We break it into eight segments for comparison, to allow for wrap-around.
+/// Modification tag, assigned incrementally.
+/// Basically just a wrapped long now.  Was more.  Can be more.  But just a long right now.
 /// </summary>
 public struct ModTag {
     long m_tag;
@@ -85,7 +115,10 @@ public struct ModTag {
     public static bool operator<= (ModTag lhs, ModTag rhs) { return lhs.m_tag <= rhs.m_tag; }
     public static bool operator> (ModTag lhs, ModTag rhs) { return lhs.m_tag > rhs.m_tag; }
     public static bool operator< (ModTag lhs, ModTag rhs) { return lhs.m_tag < rhs.m_tag; }
-
+    public static ModTag operator+ (ModTag lhs, ModTag rhs) { return new ModTag(lhs.m_tag + lhs.m_tag);}
+    public static ModTag operator- (ModTag lhs, ModTag rhs) { return new ModTag(lhs.m_tag - lhs.m_tag);}
+    public static ModTag operator++(ModTag mt) { return new ModTag(mt.m_tag + 1);}
+    public static ModTag operator--(ModTag mt) { return new ModTag(mt.m_tag - 1);}
     // The rest of this stuff is legacy from when I was accommodating our long tag being incremented past 18.4 pentillion
     // public static bool operator> (ModTag lhs, ModTag rhs) {
     //     if (DataRegistryRoot.ModTagWraps == 0) {
